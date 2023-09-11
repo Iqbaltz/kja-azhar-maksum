af0887e3add2bfc745e0f833666c2e6a8598dd43
iqbaltz <mhdiqbaltan@gmail.com>
2023-09-11 18:34:15 +0700

    fix some revision

--- components/pages/about/about.tsx
+++ components/pages/about/about.tsx
@@ -13,7 +13,7 @@ export default function About({
   data: { title, image, description1, description2 },
 }: IAboutProps) {
   return (
-    <section className="flex flex-col lg:flex-row justify-center  w-full max-w-[1080px] 2xl:max-w-[1280px] mx-auto px-4 md:px-8 2xl:px-0 pt-20 lg:pt-24 2xl:pt-40 pb-32 2xl:pb-60">
+    <section className="flex flex-col lg:flex-row justify-center  w-full max-w-[1080px] 2xl:max-w-[1280px] mx-auto px-4 md:px-8 2xl:px-0 pt-20 lg:pt-24 xl:pt-36 2xl:pt-40 pb-32">
       <div className="lg:w-1/2 lg:pr-8 mb-12 lg:mb-0 lg:mr-4">
         <h1 className="text-4xl 2xl:text-5xl questa mb-8 lg:mb-12 max-w-[400px] 2xl:max-w-[560px]">
           {title}
@@ -28,11 +28,11 @@ export default function About({
       <div className="lg:w-1/2 relative">
         <div className="relative lg:max-w-[500px]">
           <img
-            className="w-[80%] 2xl:w-full"
+            className="w-[70%] 2xl:w-[85%]"
             src={urlForImage(image).url()}
             alt="tentang"
           />
-          <div className="w-64 2xl:w-80 h-64 2xl:h-80 bg-bgprimary mix-blend-multiply bg-blend-multiply absolute -bottom-[20%] 2xl:-bottom-[30%] -right-[0] 2xl:-right-[30%]"></div>
+          <div className="w-48 2xl:w-64 h-48 2xl:h-64 bg-bgprimary mix-blend-multiply bg-blend-multiply absolute -bottom-[20%] 2xl:-bottom-[30%] right-[15%] 2xl:-right-[10%]"></div>
         </div>
       </div>
     </section>
