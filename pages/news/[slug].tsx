59f872cfaee93b440abce5e0bfb5d5e82d86c0ca
iqbaltz <mhdiqbaltan@gmail.com>
2023-09-19 22:02:58 +0700

    feat: refining news detail page

--- pages/news/[slug].tsx
+++ pages/news/[slug].tsx
@@ -1,11 +1,44 @@
 import { Hero } from "@/components/common/hero";
 import { Newsletter } from "@/components/common/newsletter";
+import { LinkedinIcon } from "@/components/common/shapes";
 import { client } from "@/sanity/lib/client";
 import { urlForImage } from "@/sanity/lib/image";
-import { getNewsBySlug, getNewsPaths } from "@/sanity/lib/queries";
+import {
+  getNewsBySlug,
+  getNewsPaths,
+  getSimpleNews,
+} from "@/sanity/lib/queries";
 import { PortableText } from "@portabletext/react";
 import { GetStaticPaths, GetStaticProps, InferGetStaticPropsType } from "next";
+import {
+  LinkedinShareButton,
+  FacebookShareButton,
+  TwitterShareButton,
+  EmailShareButton,
+} from "next-share";
+import Link from "next/link";
 import React from "react";
+
+const myBlocks: any = {
+  h1: ({ children }: { children: React.ReactNode }) => (
+    <h1 className="text-4xl">{children}</h1>
+  ),
+  h2: ({ children }: { children: React.ReactNode }) => (
+    <h2 className="text-3xl">{children}</h2>
+  ),
+  h3: ({ children }: { children: React.ReactNode }) => (
+    <h3 className="text-2xl">{children}</h3>
+  ),
+  h4: ({ children }: { children: React.ReactNode }) => (
+    <h4 className="text-xl">{children}</h4>
+  ),
+  p: ({ children }: { children: React.ReactNode }) => (
+    <p className="xl:text-sm 2xl:text-base">{children}</p>
+  ),
+  normal: ({ children }: { children: React.ReactNode }) => (
+    <p className="xl:text-sm 2xl:text-base">{children}</p>
+  ),
+};
 
 export const getStaticPaths: GetStaticPaths = async () => {
   try {
@@ -22,8 +55,12 @@ export const getStaticPaths: GetStaticPaths = async () => {
   }
 };
 
-export const getStaticProps: GetStaticProps = async ({ params }: any) => {
+export const getStaticProps: GetStaticProps = async ({
+  params,
+  locale,
+}: any) => {
   const news = await getNewsBySlug(client, params?.slug);
+  const otherNews = await getSimpleNews(client);
 
   if (!news) {
     return {
@@ -36,6 +73,8 @@ export const getStaticProps: GetStaticProps = async ({ params }: any) => {
   return {
     props: {
       news,
+      otherNews,
+      locale,
     },
     revalidate: 1,
   };
@@ -43,8 +82,12 @@ export const getStaticProps: GetStaticProps = async ({ params }: any) => {
 
 export default function NewsPage({
   news,
+  otherNews,
+  locale,
 }: InferGetStaticPropsType<typeof getStaticProps>) {
-  console.log("news", news);
+  const url = `https://kja-azhar-maksum.vercel.app/news/${news?.slug?.current}`;
+
+  console.log(locale);
   if (!news) {
     return <div>Loading...</div>;
   }
@@ -62,9 +105,42 @@ export default function NewsPage({
         })}`}
         isNews
       />
-      <section className="w-full max-w-[1080px] 2xl:max-w-[1280px] mx-auto py-24 2xl:py-32">
-        <div className="max-w-[744px]">
-          <PortableText value={news?.body} />
+      <section className="w-full max-w-[1080px] px-4 md:px-8 xl:px-0  2xl:max-w-[1280px] mx-auto py-24 2xl:py-32">
+        <div className="flex justify-between items-start">
+          <div className="max-w-[640px] 2xl:max-w-[744px]">
+            <PortableText value={news?.body} components={{ block: myBlocks }} />
+          </div>
+          <div className="bg-lightgrey px-10 py-12 ml-12 hidden xl:block">
+            <h1 className="questa text-2xl">Berita Lainnya</h1>
+            {otherNews?.map(({ title, slug }: any, i: number) => (
+              <p className="max-w-[320px] xl:text-sm 2xl:text-base mt-6">
+                <Link href={`/news/${slug}`} key={i}>
+                  {title}
+                </Link>
+              </p>
+            ))}
+          </div>
+        </div>
+        <div className="flex items-center mt-12">
+          <p>
+            {locale == "en"
+              ? "Share this article now."
+              : "Bagikan artikel ini sekarang juga."}
+          </p>
+          <div className="grid items-end grid-cols-4 gap-7 ml-12">
+            <LinkedinShareButton url={url}>
+              <img src="/images/linkedinShareIcon.png" alt="linkedin" />
+            </LinkedinShareButton>
+            <FacebookShareButton url={url}>
+              <img src="/images/facebookShareIcon.png" alt="facebook" />
+            </FacebookShareButton>
+            <TwitterShareButton url={url}>
+              <img src="/images/twitterShareIcon.png" alt="twitter" />
+            </TwitterShareButton>
+            <EmailShareButton url={url}>
+              <img src="/images/emailShareIcon.png" alt="email" />
+            </EmailShareButton>
+          </div>
         </div>
       </section>
       <Newsletter />
